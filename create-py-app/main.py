0f81b8f352cf6951058713aa013ddd54878f254f
Hideaki Yoshida <hyoshida@berkeley.edu>
2022-12-21 18:32:19 +0900

    [fix] refactor

--- create-py-app/main.py
+++ create-py-app/main.py
@@ -11,26 +11,13 @@ def main():
     base_dir = create_dir(os.getcwd(), app_name)
     with open("file_contents.yml", "r") as f:
         yaml_data = yaml.safe_load(f)
-        create_file(base_dir, "main.py", content=yaml_data["main.py"])
-        create_file(
-            base_dir,
-            "environment_variables.py",
-            content=yaml_data["environment_variables.py"],
-        )
-        create_file(base_dir, ".env", content=yaml_data[".env"])
-        create_file(
-            base_dir,
-            ".pre-commit-config.yaml",
-            content=yaml_data[".pre-commit-config.yaml"],
-        )
-        create_file(base_dir, ".gitignore", content=yaml_data[".gitignore"])
-        create_file(base_dir, "Dockerfile", content=yaml_data["Dockerfile"])
-        create_file_with_project_name(
-            base_dir, app_name, "README.md", content=yaml_data["README.md"]
-        )
-        create_file_with_project_name(
-            base_dir, app_name, "pyproject.toml", content=yaml_data["pyproject.toml"]
-        )
+        for key in yaml_data.keys():
+            if key in ["README.md", "pyproject.toml"]:
+                create_file_with_project_name(
+                    base_dir, app_name, key, content=yaml_data[key]
+                )
+                continue
+            create_file(base_dir, key, content=yaml_data[key])
     print(f"{app_name} has been created!")
 
 
